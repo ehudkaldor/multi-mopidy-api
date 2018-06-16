33a0a19a43ee2c1b4bddab99f184269a29a6bc0d
Ehud Kaldor <ehud@unfairfunction.org>
2018-06-16 11:22:28 -0700

    now actually reading the file and parsing it

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-Environment="testing"
+import configparser
+
+class Config:
+    def __init__(self, configFile):
+        config = configparser.ConfigParser()
+        config.read(configFile)
+        self.methods = config["methods"]
+        self.events_callbacks = config["events_callbacks"]
